b66d7db720eca99db20fad874eec243b1ad387d6
Prakhar Mishra <mprakhar35@gmail.com>
2022-05-17 15:41:36 +0000

    commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Button, Container, Card, Row, Text } from '@nextui-org/react';
+
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      Hello World
+      <Container sm>
+        <Card color="primary" css={{m:4}}>
+          <Row justify="center" align="center">
+            <Text h6 size={15} color="white" css={{ m: 0}}>
+              NextUI gives you the best developer experience with all the features
+              you need for building beautiful and modern websites and
+              applications.
+            </Text>
+          </Row>
+        </Card>
+      </Container>
+      <Button css={{m:5}} disabled>Hello World</Button>
     </div>
   )
 }
